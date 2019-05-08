07e52fc056cc9feb3514820ac4b0c50d14b44c49
JasonHo <Jason187928@gmail.com>
2019-05-08 14:48:39 +0800

    add student

--- src/main/java/com/jason/student/controller/StudentController.java
+++ src/main/java/com/jason/student/controller/StudentController.java
@@ -1,11 +1,14 @@
 package com.jason.student.controller;
 
+import com.jason.student.dao.ClassRoomDao;
 import com.jason.student.dao.StudentDao;
+import com.jason.student.domain.ClassRoom;
 import com.jason.student.domain.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -18,6 +21,8 @@ public class StudentController {
 
     @Autowired
     private StudentDao studentDao;
+    @Autowired
+    private ClassRoomDao classRoomDao;
 
     @GetMapping("/stus")
     public String list(Model model){
@@ -25,5 +30,20 @@ public class StudentController {
         model.addAttribute("stus",students);
 
         return "stu/list";
+    }
+
+    @GetMapping("/stu")
+    public String toAddPage(Model model){
+        Collection<ClassRoom> classRooms = classRoomDao.getClassRooms();
+        model.addAttribute("classRooms", classRooms);
+
+        return "stu/add";
+    }
+
+    @PostMapping("/stu")
+    public String addStudent(Student student){
+        System.out.println("保存 student 的信息： =====》" + student);
+        studentDao.save(student);
+        return "redirect:/stus";
     }
 }
